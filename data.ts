9da151a70b9d2bab38c45992193e769d2200bc9b
zssee <syuensee@usc.edu>
2020-09-19 01:44:41 -0500

    Update data.ts

--- data.ts
+++ data.ts
@@ -5,7 +5,7 @@ var business1 = {
   link: "https://www.hotvillechicken.com/",
   yelp: "https://www.yelp.com/biz/hotville-chicken-los-angeles-2",
   image: "https://i.imgur.com/DECOJdq.jpg",
-  tags: ["Fried Chicken", "Southern", "Sandwich", "Hot Chicken", "Black Owned Business", "Crenshaw"],
+  tags: ["Fried Chicken", "Southern", "Sandwich", "Hot Chicken", "Black-owned", "Crenshaw"],
   posts: [
     {
       image: "https://i.imgur.com/HPYMGIn.png",
@@ -29,3 +29,36 @@ var business1 = {
     },
   ],
 };
+
+var business2 = {
+  name: "ri-ri-ku",
+  address: "177 W Olympic Blvd Los Angeles, CA 90015",
+  description: "Women's clothing, cosmetics, and accessories",
+  link: "https://www.shopririku.com/",
+  yelp: "https://www.yelp.com/biz/ri-ri-ku-los-angeles?osq=Women+Owned+Businesses",
+  image: "https://i.imgur.com/MO0gTCk.jpg",
+  tags: ["Women's CLothing", "Cosmetics", "Beauty", "Boutique", "Women-owned Business", "Accessories"],
+  posts: [
+    {
+      image: "https://i.imgur.com/PWemnjr.jpg",
+      title: "Trendy pastel-colored dresses",
+      description: "Beautiful pastel-colored dresses for sale in store",
+    },
+    {
+      image: "https://i.imgur.com/GDvyuiv.jpg",
+      title: "Body Suits",
+      description: "Range of body suits and undergarments available",
+    },
+    {
+      image: "https://i.imgur.com/BOeEH48.jpg",
+      title: "Minimalist room decorations",
+      description: "Cups, bowls, and neutral-toned room decorations",
+    },
+    {
+      image: "https://i.imgur.com/Gy73Vxj.jpg",
+      title: "Accessories and Knick-Knacks",
+      description: "Handbags, headbands, books, and more to elevate your style",
+    },
+  ],
+};
+
