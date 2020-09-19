4de5d0ee32bb9c4777e71ecfe484388fb073fd00
Nathan Dimmer <nathandimmer@gmail.com>
2020-09-18 22:40:28 -0400

    chore: setting up repo and data template

---
+++ data.ts
@@ -0,0 +1,15 @@
+var business1 = {
+  name: "",
+  address: "",
+  description: "",
+  link: "",
+  image: "",
+  tags: ["tag1", "tag2"],
+  posts: [
+    {
+      image: "",
+      title: "",
+      description: "",
+    },
+  ],
+};
