2e2d1eb93ac85e2d55f63761df8ae598bc4d220d
zssee <syuensee@usc.edu>
2020-09-19 01:05:18 -0500

    Updated business 1

--- data.ts
+++ data.ts
@@ -1,15 +1,31 @@
 var business1 = {
-  name: "",
-  address: "",
-  description: "",
-  link: "",
-  image: "",
-  tags: ["tag1", "tag2"],
+  name: "Hotville Chicken & Chops LA",
+  address: "4070 Marlton Ave, Los Angeles, CA 90008",
+  description: "Casual, family-style eatery specializing in spicy fried chicken & Southern sides.",
+  link: "https://www.hotvillechicken.com/",
+  yelp: "https://www.yelp.com/biz/hotville-chicken-los-angeles-2",
+  image: "https://i.imgur.com/DECOJdq.jpg",
+  tags: ["Fried Chicken", "Southern", "Sandwich", "Hot Chicken", "Black Owned Business", "Crenshaw"],
   posts: [
     {
-      image: "",
-      title: "",
-      description: "",
+      image: "https://i.imgur.com/HPYMGIn.png",
+      title: "Spicy Fried Chicken Sandwich",
+      description: "The 'Shaw: a spicy fried chicken sandwich between toasted brioche buns, topped with pickles",
+    },
+    {
+      image: "https://i.imgur.com/vhlmNnb.png",
+      title: "Stack of Chicken Sandwiches",
+      description: "Stack of The 'Shaw Nashville Hot Chicken Sandwiches",
+    },
+    {
+      image: "https://i.imgur.com/PRFkp9J.jpg",
+      title: "Spicy Fried Chicken",
+      description: "Juicy chicken brined, floured, and fried in a secret blend of spices",
+    },
+    {
+      image: "https://i.imgur.com/uMagHA9.jpg",
+      title: "Hotville Chicken Sides",
+      description: "Hotville Chicken's Cayenne Hot Sauce and bag of chips",
     },
   ],
 };
