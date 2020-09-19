2ef771924f64ecd673e605e70d4f4c203a46dc78
Anouar <anouar.hilali1@gmail.com>
2020-09-19 18:10:42 +0100

    Change structure

--- Back End/demo/src/main/java/com/example/demo/domain/Business.java
+++ Back End/demo/src/main/java/com/example/demo/domain/Business.java
@@ -1,15 +1,13 @@
-package com.grizzyhacks.demo.domain;
+package com.example.demo.domain;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-@Document(collection = "Business")
+@Document(collection = "Businesses")
 public class Business {
+
     @Id
-    private long id;
+    private String id;
 
     private String name;
 
@@ -27,23 +25,16 @@ public class Business {
 
     //TODO: Add Post and User relations
 
-
-    public Business(long id, String name, String address, String description, String link, String yelp, String image, String location) {
-        this.id = id;
+    public Business(String name, String location) {
         this.name = name;
-        this.address = address;
-        this.description = description;
-        this.link = link;
-        this.yelp = yelp;
-        this.image = image;
         this.location = location;
     }
 
-    public long getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(String id) {
         this.id = id;
     }
 
